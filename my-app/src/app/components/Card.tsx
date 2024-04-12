c74a9e5530073865ef15bde312213b7cdeedcec7
Gabriel Fernandes <hu3master.zord@hotmail.com>
2024-04-12 15:09:59 -0300

    Terminar hero, faltando apenas um melhor code spliting

---
+++ my-app/src/app/components/Card.tsx
@@ -0,0 +1,21 @@
+import Image, { StaticImageData } from "next/image";
+
+interface Props {
+  image:StaticImageData,
+  header: string,
+  text:string
+  }
+
+  export default function Card({image,header,text} : Props) {
+  
+    return (
+      <div className="w-[328px] h-[177px]">
+      <Image src={image} width={64} height={64} alt="Design projects icon"/>
+      <h2 className="text-[#333333] text-[18px] font-bold ">{header}</h2>
+      <p className="text-[#909090] text-[18px] ">
+      {text}
+      </p>
+      </div>
+    )
+  }
+  
--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
 import smilingTeam from '../../public/smiling-creative-business-team-discussing-over-lap-566JQTQ 1.png'
 import arrow from '../../public/Arrow.png'
+import grayArrow from '../../public/grayArrow.png'
+import digitalArt from '../../public/Design.png'
+import Card from "./components/Card";
+import socialMedia from '../../public/Social Media.png'
+import webDesign from '../../public/Web Design.png'
 
 
 export default function Home() {
@@ -18,7 +23,20 @@ export default function Home() {
         Let’s Collaborate
         <Image src={arrow} width={48} height={48} alt="Let's colaborate arrow"/>
         </button>
-    <div className="bg-white absolute bottom-0 w-[1352px] h-[297px]"></div>
+    <div className="bg-white absolute bottom-0 w-[1352px] h-[297px] flex">
+
+    <button className="ml-[60px] w-[40px] h-[89px] flex flex-col items-center justify-center gap-[8px] mt-[104px]">
+      <span className="text-[18px] leading-[40px] text-[#909090] transform -rotate-90">Scroll</span>
+      <Image src={grayArrow} width={32} height={32} alt="Let's colaborate arrow"/>
+    </button>
+    
+    <article className="w-[1144px] h-[177px] mt-[60px] mb-[60px] ml-[60px] mr-[48px] flex justify-center items-center gap-[80px]">
+      <Card text="Make your website user friendly and look more professional" header="Web Design Projects" image={webDesign}></Card>
+      <Card text="Gain more followers or subscribers with the right template" header="Social Media Templates" image={socialMedia}></Card>
+      <Card text="Complete your digital work with appropriate artwork" header="Digital Artwork" image={digitalArt}></Card>
+    </article>
+
+    </div>
     </section>
 
     <Image src={smilingTeam} width={808} height={1080} alt="A Happy working team looking at a computer"/>
